b43d4e9db9a8da1316db82a21c18c39570aaaba9
Lucas Galán Arenas <133447322+LucasGA94@users.noreply.github.com>
2024-11-13 17:12:48 +0100

    script.py update formato

--- script.py
+++ script.py
@@ -6,30 +6,18 @@ respuesta = requests.get(url)
 
 if respuesta.status_code == 200:
     soup = BeautifulSoup(respuesta.text, 'html.parser')
+
     maquinas = soup.find_all('div', onclick=True)
     conteo_maquinas = 1
-    
-    """
-    autores = set()
 
-    for maquina in maquinas:
-        onclick_text = maquina['onclick']
-        autor = onclick_text.split("'")[7]
-        autores.add(autor)
-        nombre_maquina = onclick_text.split("'")[1]
-        conteo_maquinas += 1
-    """
-    
     for maquina in maquinas:
         onclick_text = maquina['onclick']
         nombre = onclick_text.split("'")[1]
         dificultad = onclick_text.split("'")[3]
         autor = onclick_text.split("'")[7]
-        
-        print(f"{nombre} --> {dificultad} --> {autor}")
-    
-    print(f"Número de maquinas encontradas: {conteo_maquinas}")
 
+        print(f"{nombre} --> {dificultad} --> {autor}")
+
+    print(f"Número de maquinas encontradas: {conteo_maquinas}")
 else:
     print(f'Error al realizar la peticion {respuesta.status_code}')
-
